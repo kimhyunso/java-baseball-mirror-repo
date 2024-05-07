cd2a42636b1f6524a0c2a23cd204eab764b4d55d
kimhyunso <gustn9515@gmail.com>
2024-05-07 18:01:45 +0900

    testCode write

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,5 +9,28 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+        // assertThat(actual).isEqualTo("test");
     }
+    @Test
+    public void splitTest(){
+        String input = "1,2";
+        String splitResult[] = input.split(",");
+        assertThat(splitResult).contains("1");
+        assertThat(splitResult).containsExactly("1", "2");
+    }
+
+    @Test
+    public void substringTest(){
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    public void charAtTest(){
+        String input = "abc";
+    }
+
+
+
 }
