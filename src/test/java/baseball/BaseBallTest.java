5ed1710ea3310d91e522272f46d5ec360731b874
kimhyunso <gustn9515@gmail.com>
2024-05-11 22:21:27 +0900

    feat(test): 유효성 검사 입력값 세자리인지 판별하는 기능 구현

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -12,10 +12,27 @@ public class BaseBallTest {
     @Test
     @DisplayName("유효성검사 입력값 1~9")
     void 유효성검사_입력값이1에서9까지(){
-        assertThat(ValidationCheck.valid(1)).isTrue();
-        assertThat(ValidationCheck.valid(0)).isFalse();
-        assertThat(ValidationCheck.valid(10)).isFalse();
-        assertThat(ValidationCheck.valid(-20)).isFalse();
+        assertThat(ValidationCheck.isCorrect(1)).isTrue();
+        assertThat(ValidationCheck.isCorrect(9)).isTrue();
+        assertThat(ValidationCheck.isCorrect(0)).isFalse();
+        assertThat(ValidationCheck.isCorrect(10)).isFalse();
+        assertThat(ValidationCheck.isCorrect(-20)).isFalse();
+    }
+
+    @DisplayName("유효성 검사 입력값이 세자리인지 판별하기")
+    @ParameterizedTest
+    @ValueSource(strings = "1232432")
+    void 유효성검사_세자리판별_False(String input){
+        boolean result = ValidationCheck.isThreeWord(input);
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("유효성 검사 입력값이 세자리인지 판별하기")
+    @ParameterizedTest
+    @ValueSource(strings = "123")
+    void 유효성검사_세자리판별_True(String input){
+        boolean result = ValidationCheck.isThreeWord(input);
+        assertThat(result).isTrue();
     }
 
 }
