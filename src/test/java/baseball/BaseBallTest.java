e400993aca81090ec24655fbf59ade950df42e40
kimhyunso <gustn9515@gmail.com>
2024-05-13 10:35:46 +0900

    test(Ball): BaseBallTest refactor

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BaseBallTest {
 
+    private Ball ball;
+
+    @BeforeEach
+    void setUp(){
+        ball = new Ball(1, 2);
+    }
+
+
     @DisplayName("입력된 숫자가 일치하지 않는 낫싱")
     @Test
     void nothing(){
-        Ball ball = new Ball(1, 2);
         BaseBallStatus status = ball.play(new Ball(1, 5));
         assertThat(status).isEqualTo(BaseBallStatus.NOTHING);
     }
@@ -18,7 +26,6 @@ public class BaseBallTest {
     @DisplayName("입력된 숫자가 값은 같은데 위치가 다른 볼")
     @Test
     void ball(){
-        Ball ball = new Ball(1, 2);
         BaseBallStatus status = ball.play(new Ball(2, 2));
         assertThat(status).isEqualTo(BaseBallStatus.BALL);
     }
@@ -26,7 +33,6 @@ public class BaseBallTest {
     @DisplayName("입력된 숫자와 위치가 같은 스트라이크")
     @Test
     void strike(){
-        Ball ball = new Ball(1, 2);
         BaseBallStatus status = ball.play(new Ball(1, 2));
         assertThat(status).isEqualTo(BaseBallStatus.STRIKE);
     }
