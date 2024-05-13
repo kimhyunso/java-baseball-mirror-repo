69e212b2ca1127d5d6db68b7f99b135b54c4a1a0
kimhyunso <gustn9515@gmail.com>
2024-05-13 10:14:06 +0900

    feat(VaildationCheck): 입력된 숫자가 1~9까지의 숫자인지 판별하는 기능 구현

--- src/main/java/baseball/ValidationCheck.java
+++ src/main/java/baseball/ValidationCheck.java
@@ -3,7 +3,14 @@ package baseball;
 public class ValidationCheck {
 
 
+    private static final int MIN_NO = 0;
+    private static final int MAX_NO = 10;
+
     public static boolean isThreeWord(String input) {
         return input.length() == 3;
+    }
+
+    public static boolean isCorrect(int no) {
+        return no > MIN_NO && no < MAX_NO;
     }
 }
--- src/test/java/baseball/ValidationCheckTest.java
+++ src/test/java/baseball/ValidationCheckTest.java
@@ -9,20 +9,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ValidationCheckTest {
 
-    @DisplayName("입력된 숫자가 3인지 판별하기")
-    @ParameterizedTest
-    @ValueSource(strings = "12")
-    void 입력된숫자가3인가_False(String input){
-        boolean result = ValidationCheck.isThreeWord(input);
-        assertThat(result).isFalse();
-    }
 
     @DisplayName("입력된 숫자가 3인지 판별하기")
-    @ParameterizedTest
-    @ValueSource(strings = "123")
-    void 입력된숫자가3인가_True(String input){
-        boolean result = ValidationCheck.isThreeWord(input);
-        assertThat(result).isTrue();
+    @Test
+    void 입력된숫자가3인가(){
+        assertThat(ValidationCheck.isThreeWord("123")).isTrue();
+        assertThat(ValidationCheck.isThreeWord("12")).isFalse();
+    }
+
+
+    @DisplayName("입력된 숫자가 1~9까지의 숫자인지 판별하기")
+    @Test
+    void 입력된숫자_1_9(){
+        assertThat(ValidationCheck.isCorrect(1)).isTrue();
+        assertThat(ValidationCheck.isCorrect(9)).isTrue();
+        assertThat(ValidationCheck.isCorrect(0)).isFalse();
+        assertThat(ValidationCheck.isCorrect(10)).isFalse();
     }
 
 
